1f36307ecea6297628da26da99ae1deee5712a65
Aaron <williamsa493@strose.edu>
2017-02-03 13:02:55 -0500

    made changes to this file, have nice day
    -Aaron

--- SoftEngLab1/src/hello.java
+++ SoftEngLab1/src/hello.java
@@ -4,5 +4,9 @@ public static void main(String[] args){
 	System.out.println("hello rich");
 	System.out.println("in the terminal");
 	System.out.println("still in the terminal");
+	System.out.println("Aaron was here!!!");
+	System.out.println("Have nice day");
+
+
 }
 }
